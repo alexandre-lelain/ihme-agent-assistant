4afd44f1d0cfb549862711cfd9173db42c8f8116
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2017-11-12 13:26:32 +0100

    added simple hello world response

--- server/agent_assistant/api/views.py
+++ server/agent_assistant/api/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 def home(request):
     return render(request, 'api/home.html')
+
+def say_hi(request):
+    return JsonResponse({"response" : "Hello there !"})
