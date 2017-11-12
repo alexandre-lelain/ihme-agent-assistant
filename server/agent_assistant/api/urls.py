47a8097e0c6c513abf1fd33458341f73f58a4a87
Gautier DARCHEN <gautier.darchen@insa-rouen.fr>
2017-11-12 10:50:01 +0100

    Initialisation du projet Django de l'API (home, urls, settings...) + modification des requirements

---
+++ server/agent_assistant/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import reverse_lazy
+
+from . import views
+
+app_name= 'api'
+
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+]
--- server/agent_assistant/conf/urls.py
+++ server/agent_assistant/conf/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include('api.urls'))
 ]
